d315d923a9a0feb19910c8a1bca19be2fecd2863
Jirayu Limjinda <jir4yu.l@gmail.com>
2018-12-30 11:27:02 +0700

    update theme options

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section class="main-content">
+	<div class="notfound-block">
+		<h1><?php _e('ไม่พบหน้าที่คุณต้องการเรียกดู', 'jindatheme'); ?></h1>
+    <p><?php _e('หน้านี้อาจจะถูกย้าย หรือลบออกไปแล้วออกจากเว็บไซต์ ลองกลับไปที่หน้าแรกเพื่อดูอะไรอื่นๆแทน', 'jindatheme'); ?></p>
+	</div>
+</section>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -2,20 +2,28 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 	<head>
-	  <meta charset="utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/img/favicons/apple-touch-icon.png">
-	  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-32x32.png">
-	  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-16x16.png">
-	  <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/img/favicons/manifest.json">
-	  <link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/img/favicons/safari-pinned-tab.svg" color="#eac040">
-	  <meta name="theme-color" content="#ffffff">
+	  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<link rel="profile" href="https://gmpg.org/xfn/11" />
+		<link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon.ico" />
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/android-chrome-192x192.png" sizes="192x192" />
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri()?>/img/favicons/favicon-16x16.png" sizes="16x16" />
+		<link rel="manifest" href="<?php echo get_template_directory_uri()?>/img/favicons/manifest.json" />
+		<link rel="mask-icon" href="<?php echo get_template_directory_uri()?>/img/favicons/safari-pinned-tab.svg" />
+		<?php if ( is_single() ) { ?>
+			<link rel="prefetch" href="<?php echo bloginfo('url'); ?>">
+		<?php } ?>
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="theme-color" content="#ffffff">
 		<?php wp_head(); ?>
+		<?php the_field('header_scripts', 'option'); ?>
 	</head>
 	<body <?php body_class() ?>>
-		
+		<?php the_field('body_scripts', 'option'); ?>
 		<div class="wrapper">
 			<main class="main">
 				<div class="container page-wrapper">
